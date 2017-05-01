ce159cf3cc243e225022cf5f2f45abb73612d312
denisveselov <vorle57@gmail.com>
2017-05-01 03:41:12 +0300

    v.0.0.2
    Working bot
    *Значения переменных пакета Update записываются в таблицу
    *Бот отвечает на базовые вопросы

---
+++ global_vars.php
@@ -0,0 +1,20 @@
+<?php
+mb_internal_encoding("UTF-8");
+
+//Get Update
+$update = json_decode(file_get_contents('php://input'),true);
+$chat_id = $update['message']['chat']['id'];
+$message_id = $update['message']['message_id'];
+$user_name = $update['message']['from']['username'];
+$message = $update['message']['text'];
+$message = mb_strtolower($message);
+
+
+//create to DB
+$usermsg = R::dispense('usermsg');
+$usermsg->chat_id = $chat_id;
+$usermsg->msgid = $message_id;
+$usermsg->user_name = $user_name;
+$usermsg->message = $message;
+$id = R::store($usermsg);
+
